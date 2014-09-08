78bf3ba3f828f0f4530b91f2509f847cc0ace88d
Erika Suárez Valencia <marvinesv@gmail.com>
2014-09-08 15:18:26 -0500

    Otro formato para guardar los grafos

--- src/Graph6Extension.java
+++ src/Graph6Extension.java
@@ -4,5 +4,6 @@ import org.nlogo.api.PrimitiveManager;
 public class Graph6Extension extends DefaultClassManager {
   public void load(PrimitiveManager primitiveManager) {
     primitiveManager.addPrimitive("save-graph6", new Graph6Command());
+    primitiveManager.addPrimitive("save-csv", new CsvCommand());
   }
 }
